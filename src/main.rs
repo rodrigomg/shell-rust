4cb542c1aba82a365cbeb9fd3f1b9b257b9b7bd0
technosoul <rockdrigo.mtz@gmail.com>
2018-10-26 16:26:57 -0500

    Creating block_1 and reading a line from standard input

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::io;
+fn main() {
+    loop {
+        println!(">_ ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)
+                    .expect("Failed to read line");
+        println!("{:?}", input);
+    }
+}
