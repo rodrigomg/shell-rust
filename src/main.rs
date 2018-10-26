d718110eb6cbcc699d5f21a2d6c7b49c7b96ce1d
technosoul <rockdrigo.mtz@gmail.com>
2018-10-26 17:24:58 -0500

    Reading input with it's arguments, parsing to command line and execute it

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
 use std::io;
+use std::process::Command;
 fn main() {
     loop {
         println!(">_ ");
         let mut input = String::new();
         io::stdin().read_line(&mut input)
-                    .expect("Failed to read line");
-        println!("{:?}", input);
+            .expect("Failed to read line");
+        let words: Vec<&str> = input.split_whitespace().collect();
+        let mut iterator = words.into_iter();
+        //list_dir.status().expect("process failed to execute");
+        //println!("{:?}", words);
+        //println!("{:?}", iterator.next());
+        let mut list_dir;
+        match iterator.next() {
+            Some(command) => list_dir = Command::new(command),
+            None => list_dir = Command::new("clear")
+        }
+        for e in iterator {
+            list_dir.arg(e);
+        }
+        list_dir.status().expect("Fallo");
     }
+    /* let output = Command::new("sh")
+       .arg("-c")
+       .arg("echo hello")
+       .output()
+       .expect("failed to execute process");
+       let hello = output.stdout;
+       println!("{:?}",hello);*/
 }
