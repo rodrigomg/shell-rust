e8874ce9fa3cc01e049164be2dae39543212efe8
technosoul <rockdrigo.mtz@gmail.com>
2018-10-26 18:30:05 -0500

    Adding match with spawn to validate command line

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,33 @@
 use std::io;
+use std::io::Write;
 use std::process::Command;
+
 fn main() {
     loop {
-        println!(">_ ");
+        print!(">_ ");
+        io::stdout().flush().ok().expect("Could not flush stdout");
         let mut input = String::new();
         io::stdin().read_line(&mut input)
             .expect("Failed to read line");
         let words: Vec<&str> = input.split_whitespace().collect();
         let mut iterator = words.into_iter();
-        let mut list_dir;
+        let mut command;
         match iterator.next() {
-            Some(command) => list_dir = Command::new(command),
-            None => list_dir = Command::new("clear")
+            Some(arg) => command = Command::new(arg),
+            None => command = Command::new("clear")
         }
         for e in iterator {
-            list_dir.arg(e);
+            command.arg(e);
         }
-        list_dir.status().expect("Fallo");
+        match command.spawn() {
+            Ok(_) => println!("cool"),
+            Err(e) => println!("{:?}", e),
+
+        }
+        //let status = command.status().expect("Fallo");
+        //match status.code() {
+        //    Some(code) => println!("Exited with status code: {}", code),
+        //    None       => println!("Process terminated by signal")
+        //}
     }
 }
